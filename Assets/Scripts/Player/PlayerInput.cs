35d595952427c4a5b6a0de6abb8c90f5236a19f8
smwater <smwater130@naver.com>
2023-02-06 13:04:36 +0900

    Feat: 모드 전환 구현

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public bool IsClick { get; private set; }
+    public bool RotationMode { get; private set; }
+    public bool CrushMode { get; private set; }
+    public bool ProtectMode { get; private set; }
 
     private void Update()
     {
         IsClick = Input.GetMouseButtonDown(0);
+
+        if (!CrushMode && !ProtectMode)
+        {
+            RotationMode = true;
+        }
+        else
+        {
+            RotationMode = false;
+        }
+
+        CrushMode = Input.GetKey(KeyCode.Q);
+        ProtectMode = Input.GetKey(KeyCode.E);
     }
 }
