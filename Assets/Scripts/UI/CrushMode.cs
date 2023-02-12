f78e6bdc27a6dee1a5c2584496d1a3ba9ebcc0be
Hyunsoo Park <smwater130@naver.com>
2023-02-13 08:04:24 +0900

    Feat: 스프라이트 전환하는 함수 구현

---
+++ Assets/Scripts/UI/CrushMode.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrushMode : MonoBehaviour
+{
+    [SerializeField] private Sprite _modeOnSprite;
+
+    private Image _image;
+    private Sprite _modeOffSprite;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+        _modeOffSprite = _image.sprite;
+    }
+
+    /// <summary>
+    /// 모드가 켜졌을 때의 스프라이트로 전환하는 함수
+    /// </summary>
+    public void On()
+    {
+        _image.sprite = _modeOnSprite;
+    }
+
+    /// <summary>
+    /// 모드가 꺼졌을 때의 스프라이트로 전환하는 함수
+    /// </summary>
+    public void Off()
+    {
+        _image.sprite = _modeOffSprite;
+    }
+}
