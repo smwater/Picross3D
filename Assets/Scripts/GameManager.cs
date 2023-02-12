eb21c18b3d19f9c0b5a1473864b85242ad6f9847
Hyunsoo Park <smwater130@naver.com>
2023-02-13 07:56:54 +0900

    Chore: 사용할 이벤트 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : SingletoneBehaviour<MonoBehaviour>
 {
-
+    public UnityEvent ModeOff = new UnityEvent();
+    public UnityEvent CrushModeOn = new UnityEvent();
+    public UnityEvent ProtectModeOn = new UnityEvent();
 }
